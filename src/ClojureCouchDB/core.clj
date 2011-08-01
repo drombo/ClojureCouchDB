96350f5a1ebc33dc9b384a30fdb6f002fcb4fe1c
Sebastian Breit <sbr@mms-dresden.de>
2011-08-01 21:17:13 +0200

    erste funktionierende version

--- src/ClojureCouchDB/core.clj
+++ src/ClojureCouchDB/core.clj
@@ -1,7 +1,10 @@
 (ns ClojureCouchDB.core
-  (:import (org.jcouchdb.db Database)))
+  (:import (org.jcouchdb.db Database))
+  (:require clojure.contrib.logging))
 
-(def db (Database. ("localhost", "sbrdb")))
-(type db)
+(def db (Database. "localhost" "sbrdb"))
 
-;;(def doc 
+(def adoc (doto (java.util.HashMap.) (.put "k1" "v1") (.put "k2" "v2")))
+
+(.createDocument db adoc)
+
